f1fca5e0b036b222beb833de2252f694acefff82
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2021-03-26 15:38:17 -0300

    CM - Adds search by type of Pokemon
    Adiciona pesquisa pelo tipo de Pokemon

--- pokemon_redfox/lib/pages/pokemon_list_page.dart
+++ pokemon_redfox/lib/pages/pokemon_list_page.dart
@@ -9,7 +9,7 @@ class PokemonListPage extends StatefulWidget {
 
 class _PokemonListPageState extends State<PokemonListPage> {
   PokemonService pokemonService;
-
+  String _search;
   @override
   void initState() {
     super.initState();
@@ -24,43 +24,70 @@ class _PokemonListPageState extends State<PokemonListPage> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: FutureBuilder<PokemonResultModel>(
-              future: pokemonService.getPokemons(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState != ConnectionState.done) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-
-                if (snapshot.hasError) {
-                  return Center(
-                    child: Text("Ops algo deu errado!"),
-                  );
-                }
-
-                if (!snapshot.hasData) {
-                  return Center(
-                    child: Text("Ops não temos nada para exibir!"),
-                  );
-                }
-
-                return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: snapshot.data.results.length,
-                    itemBuilder: (_, index) {
-                      final item = snapshot.data.results[index];
-                      return ListTile(
-                        title: Text("${item?.detail?.name}"),
-                        leading: Image.network(item.getImage),
-                        subtitle: Text("Habilidades: ${item.getAbilities}"),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(18.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Pesquisa',
+                  labelText: 'Pesquisa',
+                  border: OutlineInputBorder(),
+                ),
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                ),
+                onSubmitted: (text) {
+                  setState(() {
+                    _search = text;
+                  });
+                },
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height,
+              child: FutureBuilder<PokemonResultModel>(
+                  future: pokemonService.getPokemons(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState != ConnectionState.done) {
+                      return Center(
+                        child: CircularProgressIndicator(),
                       );
-                    });
-              }),
+                    }
+
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text("Ops algo deu errado!"),
+                      );
+                    }
+
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: Text("Ops não temos nada para exibir!"),
+                      );
+                    }
+                    return buildListViewPokemon(snapshot);
+                  }),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  ListView buildListViewPokemon(AsyncSnapshot<PokemonResultModel> snapshot) {
+    return ListView.builder(
+        padding: EdgeInsets.only(right: 18.0, left: 18.0),
+        shrinkWrap: true,
+        itemCount: snapshot.data.results.length,
+        itemBuilder: (_, index) {
+          final item = snapshot.data.results[index];
+          return ListTile(
+            title: Text("${item?.detail?.name}"),
+            leading: Image.network(item.getImage),
+            subtitle: Text("Habilidade: ${item.getAbilities}"),
+          );
+        });
   }
 }
