bd77cc7ca283ad6a45a8feff0a94e1d41acb8be6
Biel <bielp07@gmail.com>
2021-04-09 16:24:13 -0300

    Setup project

---
+++ desafioPokemon/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import './src/Components/List.js'
+
+export default App;
