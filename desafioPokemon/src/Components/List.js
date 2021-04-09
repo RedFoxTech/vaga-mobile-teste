3592958a3d66f39bd78561b6ab6a06609bd52282
Biel <bielp07@gmail.com>
2021-04-09 20:05:36 -0300

    Fetch Pokemons and Images from APIs and show in the screen. Also, set navigation to 'Details' screen when a Pokemon is clicked.

--- desafioPokemon/src/Components/List.js
+++ desafioPokemon/src/Components/List.js
@@ -1,31 +1,81 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
 	View,
 	Text,
 	FlatList,
 	ListItem,
 	TouchableOpacity,
+	StyleSheet,
+	ScrollView,
+	Image
 } from 'react-native';
 
-class List extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			data: [],
-			loading: false,
-			error: null,
-			search: null,
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/"
 
-		};
-	}
+const List = ({navigation}) => {
+    useEffect(() => {
+	getPokes()
+    }, [])
 
-	render() {
-		return(
-			<View>
-				<Text>List component = Home!</Text>
-			</View>
-		)
-	}
+    const [pokes, setPokes] = useState([])
+    const [search, setSearch] = useState('')
+
+    const getPokes = () => {
+	fetch(apiUrl)
+	.then(response => response.json())
+	.then(pokes => setPokes(pokes.results))
+    }
+
+    const pokeId = (id) => {
+	id = id.toString()
+	const paddednum = id.padStart(3, '0');
+	return paddednum
+    }
+
+    //@@@FIXME(0xbiel): A URL da imagem está sendo repetida porque
+    //quando colocada em uma variável, a variável 'index'
+    //deixa de existir, retornando um erro.
+
+    return (
+	<View> 
+	    <TouchableOpacity
+		style={styles.pokeball}
+		activeOpacity={0.5}
+		onPress={() => navigation.navigate('Profile')}>
+	    </TouchableOpacity>
+	    <ScrollView style={styles.pokeWrapper}>
+		<View style={styles.container}>
+		    {pokes.map((pokemon, index) => {
+			return (
+			    <TouchableOpacity
+				key={index}
+				onPress={() =>
+				    navigation.navigate('Details', {
+				    pokemon: pokemon.name,
+				    //PokeAPI não oferece imagens.
+				    //Pegando do site oficial do Pokemon.
+				    image: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokeId(index + 1,)}.png`,
+				})
+			    }>
+			    <Image
+				style={{width: 100, height: 100}}
+				source={{
+				//PokeAPI não oferece imagens.
+				//Pegando do site oficial do Pokemon.
+				uri: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokeId(index + 1,)}.png`,
+				}}
+			    />
+			    <Text>{pokemon.name}</Text>
+			    </TouchableOpacity>
+		    );
+		})}
+	    </View>
+      </ScrollView>
+    </View>
+    )
 }
 
+
 export default List;
+
+const styles = StyleSheet.create({})
