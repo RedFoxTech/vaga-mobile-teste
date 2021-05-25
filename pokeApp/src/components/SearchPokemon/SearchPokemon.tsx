244bf6727657a1fb4c856f5141e3f14c2a20b015
marcelorafael <marcelorafael23@hotmail.com>
2021-05-25 08:45:04 -0300

    feat: search ok

--- pokeApp/src/components/SearchPokemon/SearchPokemon.tsx
+++ pokeApp/src/components/SearchPokemon/SearchPokemon.tsx
@@ -3,47 +3,58 @@ import React from 'react';
 import {
   Image,
   Text,
-  View
+  TextInput,
+  View,
+  Button
 } from 'react-native';
 import api from '../../services/api';
 
 const SearchPokemon = () => {
   const [data, setData] = React.useState<any[]>([])
+  const [pokemonName, setPokemonName] = React.useState('')
 
   let pokemon: any = [];
+  async function loadData() {
+      
+    try {
+      const response = await api.get(`/pokemon/${pokemonName.toLocaleLowerCase()}`)
+
+      const pokemonAdapter = () => {
+        return [{
+          abilities: response.data.abilities[1].ability.name,
+          name: response.data.name,
+          img: response.data.sprites["front_default"],
+          type: response.data.types[0].type.name,
+          weight: response.data.weight,
+          height: response.data.height 
+        }]
+      }                                                                                 
+
+      pokemon = pokemonAdapter()
+
+      setData(pokemon)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   React.useEffect(() => {
-    async function loadData() {
-      
-      try {
-        const response = await api.get('/pokemon/pikachu')
-
-        const pokemonAdapter = () => {
-          return [{
-            abilities: response.data.abilities[1].ability.name,
-            name: response.data.name,
-            img: response.data.sprites["front_default"],
-            type: response.data.types[0].type.name,
-            weight: response.data.weight,
-            height: response.data.height 
-          }]
-        }                                                                                 
-
-        pokemon = pokemonAdapter()
-        
-        setData(pokemon);
-        console.log(data[0])
-      } catch (error) {
-        console.log(error)
-      }
-    }
+    
     loadData();
   },[]);
   
   
   return (
-    <View style={{flex: 1, alignItems:'center', justifyContent:'center', backgroundColor:"#FFF"}}>
-       
+    <View style={{flex: 1, alignItems:'center', justifyContent:'center', backgroundColor:"#ff0000"}}>
+       <TextInput
+        placeholderTextColor="#000"
+        style={{borderWidth: 1, width: '80%', borderRadius: 5, backgroundColor:"#FF2548"}}
+        placeholder="Search here..."
+        onChangeText={value => setPokemonName(value)}
+        value={pokemonName}
+       />
+       <Button title="Search" onPress={() => loadData()} />
        {data.map(item => {
          return(
            <View>
