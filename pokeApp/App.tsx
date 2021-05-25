82860a07ba186b4734787973d290f9ccec07ff42
marcelorafael <marcelorafael23@hotmail.com>
2021-05-25 11:04:26 -0300

    feat: Datas rendering

--- pokeApp/App.tsx
+++ pokeApp/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
 
   return (
     <>
-      <SearchPokemon />
+      <AllPokemons />
     </>
   );
 };
--- pokeApp/src/components/AllPokemons/AllPokemons.tsx
+++ pokeApp/src/components/AllPokemons/AllPokemons.tsx
@@ -1,38 +1,46 @@
 import React from 'react'
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, Image } from 'react-native'
 import api from '../../services/api';
 
 export default function AllPokemons() {
-    const [pokemons, setPokemons] = React.useState([])
-
+    const [pokemons, setPokemons] = React.useState<any[]>([])
+    
     React.useEffect(() => {
         async function loadData() {
-            const response = await api.get('/pokemon?limit=100');
-            setPokemons(response.data)
+            try {
+                const response = await api.get('/pokemon?limit=100');
 
-            console.log(response.data)
+            
+                setPokemons(response.data.results)
+
+                // console.log(pokemons)
+            } catch (error) {
+                console.log(error)
+            }
         }
         loadData();
     }, []);
 
     return (
         <View style={{ flex: 1, backgroundColor: '#FFF' }}>
-            <Text>Teste</Text>
+            
             <FlatList
                 data={pokemons}
                 keyExtractor={item => item.name}
-                renderItem={PokemonShow}
+                renderItem={({item}) => {
+                    const {name, url} = item
+                    const URL = url.replace('https://pokeapi.co/api/v2/pokemon/','').replace('/','')
+                    const urlImage = `https://pokeres.bastionbot.org/images/pokemon/${URL}.png`
+                    
+                    return(
+                        <View>
+                            <Image source={{uri: urlImage}} style={{width: 50, height: 50}}/>
+                            <Text> {name} </Text>
+                        </View>
+                    );
+                }}
             />
-        </View>
-    )
-}
-
-function PokemonShow(item) {
-
-
-    return (
-        <View>
-            <Text>{item.name}</Text>
+            
         </View>
     )
 }
